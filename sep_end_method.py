f0d45a53f0ef4c9458384598512484cb786be575
Bhargavimadipoju <104247048+Bhargavimadipoju@users.noreply.github.com>
2022-05-23 10:30:14 +0530

    Add files via upload

---
+++ sep_end_method.py
@@ -0,0 +1,42 @@
+"""date:26-april-2022
+name:bhargavi
+fiel desc:sep_end_method
+time:3:40
+"""
+# print('a'+'b')
+# print('a','b')
+# print('a '+''+ 'b')
+
+# print(''.isspace())
+# print('b'.isspace())
+# print('   '.isspace())
+# print('          '.isspace())
+
+# print(''+'hi')
+# print(' '+'hi')
+# print(' ','hi')
+
+# print('python',3.10)
+# print('python',3.18)
+# print('python',3.28)
+
+# print('python'+'4')
+# print('python'+str(4))
+# print('python-'+str(7))
+
+# print(1,2,3)
+# print('1'+'-''+''2'+'-''3')
+
+# print(1,2,3,sep='')
+# print(1,2,3,sep='-')
+# print(1,2,3,sep='  ')
+# print(1,2,3,sep='@')
+
+# print('p' 'y')
+# print('p','y')
+# print('p','y',sep='@')
+# print(10,20,30,70,sep='&')
+# print(10,20,30,70,sep='&',end='&')
+
+# print('hi',end='')
+# print('bye')
